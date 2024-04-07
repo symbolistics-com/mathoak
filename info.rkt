aef8a0bcd0539318ba79d0dacb9edb32d8d2aef5
KDr2 <zhuo.dev@gmail.com>
2024-04-07 10:26:20 +0800

    first commit

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "MathOak")
+(define deps '("base" "srfi"))
+;; (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "MathOak, tools and library for practicle mathematics.")
+(define version "0.0.1")
+(define pkg-authors '(Symbolistics.com Killian Josh))
